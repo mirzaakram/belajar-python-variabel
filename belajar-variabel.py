ee10d18b5f0befd244bc857722154553cfe035a7
mirzaakram <zeinspace30@gmail.com>
2025-10-31 08:32:28 +0700

    Add files via upload

---
+++ belajar-variabel.py
@@ -0,0 +1,31 @@
+#variabel
+nama = "mirza"
+mapel_fav = 'seni budaya'
+nilai = 90
+laki_laki = True
+
+print ("====Data Nilai====")
+print ("nama: %s" % (nama))
+print ("mapel_fav: %s, nilai: %d, laki_laki: %r" %(mapel_fav, nilai, laki_laki))
+
+#tugas
+Barang_1 = "Roti"
+Harga_1 = 10000
+Barang_2 = "susu"
+Harga_2 = 15000
+Barang_3 = "selai"
+Harga_3 = 17000
+Barang_4 = "gula"
+Harga_4 = 22000
+Barang_5 = "kopi"
+Harga_5 = 8000
+total = Harga_5 + Harga_2
+
+print ("========selamat datang di alfamart==========")
+print("1.%s : Rp%d" % (Barang_1, Harga_1))
+print("2.%s : Rp%d" % (Barang_2, Harga_2))
+print("3.%s : Rp%d" % (Barang_3, Harga_3))
+print("4.%s : Rp%d" % (Barang_4, Harga_4))
+print("5.%s : Rp%d" % (Barang_5, Harga_5))
+print("Total belanja: kopi + susu = Rp%s" %(total))
+
